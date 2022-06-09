0175f48f04d6a1bf656495c6cf4d6826816828e1
MalikMuhammadAli56 <96606674+MalikMuhammadAli56@users.noreply.github.com>
2022-06-09 12:42:12 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Box
+{
+  int width,height,depth;
+//System.out.println("Width:" + width+ "Height:"+height + "Depth:"+depth);
+}
+
+public class Main
+{
+  public static void main(String a[])  
+  {
+      Box b=new Box();
+
+	System.out.println("Width:" + b.width+ "\nHeight:"+b.height + "\nDepth:"+b.depth);
+
+  }
+
+
+}
